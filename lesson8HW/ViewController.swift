f18705ea3eacdcc86cdbcecc133b8fd44074d6f6
mfcodes <86621630+mfcod@users.noreply.github.com>
2023-12-31 14:41:11 +0200

    Initial commit

---
+++ lesson8HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson8HW
+//
+//  Created by Maxim Fedoseenko on 31.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
